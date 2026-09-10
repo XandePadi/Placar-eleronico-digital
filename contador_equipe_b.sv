a4a8c63d0633c205d74a1d9f3d7dba17de3a6ee9
Alexandre Padilha <xande.u.padilha@gmail.com>
2026-09-10 12:08:10 -0300

    Refactor contador_equipe_b module and logic

--- contador_equipe_b.sv
+++ contador_equipe_b.sv
@@ -1,27 +1,108 @@
-module contador_equipe_b (
-    input clk,
-    input rst,
+module contador_equipe_B (
+    input logic clk,
+    input logic rst,
 
-    input incr_b,
-    input decr_b
+    input logic incr_b,
+    input logic decr_b,
 
-    output numero_de_pontos
+    output logic [3:0] unidade_B,
+    output logic [3:0] dezena_B,
+    output logic [3:0] centena_B,
+    output logic [3:0] milhar_B
 );
 
-    detector_de_borda equipe_a(
-        .clock(clk),
-        .reset(rst),
-        .borda_incr_a(incr_b),
-        .borda_decr_a(decr_b)
+    logic borda_incr_b;
+    logic borda_decr_b;
+
+    detector_de_borda equipe_b (
+        .clock        (clk),
+        .reset        (rst),
+        .incr_b       (incr_b),
+        .decr_b       (decr_b),
+        .borda_incr_b (borda_incr_b),
+        .borda_decr_b (borda_decr_b)
     );
 
-    always_ff @(posedge clk or posedge rst)begin
-        if(rst)
-            numero_de_pontos <= '0;
-        else if(incr_b)
-            numero_de_pontos <= numero_de_pontos + 1;
-        else if(decr_b)
-            numero_de_pontos <= numero_de_pontos - 1;
+    always_ff @(posedge clk or posedge rst) begin
+
+        if (rst) begin
+            unidade_B <= 4'd0;
+            dezena_B  <= 4'd0;
+            centena_B <= 4'd0;
+            milhar_B  <= 4'd0;
+        end
+
+        else if (borda_incr_b) begin
+
+            if (unidade_B == 9) begin
+                unidade_B <= 0;
+
+                if (dezena_B == 9) begin
+                    dezena_B <= 0;
+
+                    if (centena_B == 9) begin
+                        centena_B <= 0;
+
+                        if (milhar_B == 9) begin
+                            milhar_B <= 9; // milhar fica parado no 9
+                        end
+                        else begin
+                            milhar_B <= milhar_B + 1;
+                        end
+
+                    end
+                    else begin
+                        centena_B <= centena_B + 1;
+                    end
+
+                end
+                else begin
+                    dezena_B <= dezena_B + 1;
+                end
+
+            end
+            else begin
+                unidade_B <= unidade_B + 1;
+            end
+
+        end
+
+        else if (borda_decr_b) begin
+            if (unidade_B == 0 && dezena_B == 0 && centena_B == 0 && milhar_B == 0) begin
+                // já está zerado, não decrementa
+                unidade_B <= 0;
+                dezena_B  <= 0;
+                centena_B <= 0;
+                milhar_B  <= 0;
+            end
+
+            else if (unidade_B == 0) begin
+                unidade_B <= 9;
+
+                if (dezena_B == 0) begin
+                    dezena_B <= 9;
+
+                    if (centena_B == 0) begin
+                        centena_B <= 9;
+                        milhar_B  <= milhar_B - 1; // aqui sabemos que milhar > 0
+                    end
+                    else begin
+                        centena_B <= centena_B - 1;
+                    end
+
+                end
+                else begin
+                    dezena_B <= dezena_B - 1;
+                end
+
+            end
+            else begin
+                unidade_B <= unidade_B - 1;
+            end
+
+        end
     end
+
+    
 
 endmodule
